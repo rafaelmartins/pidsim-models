e50fda8ca8c80b1fec57a2e24ce6f64bed17b73d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-10-30 15:09:15 -0200

    fixed README.rst; added setup.py

--- README.rst
+++ README.rst
@@ -1,5 +1,29 @@
-PIDSIM models
-=============
+PIDSIM reference models package
+===============================
 
-This package implements a set of reference models to be used
-with PIDSIM.
+.. warning::
+
+   This package just have the python modules, not any GUI.
+
+This package implements a basic set of reference models for the simulation
+of PID controllers, using PIDSIM.
+
+If you need some help with the use, or can help with the development,
+please contact the author via email or visit our project website:
+
+http://pidsim.org/
+
+All the help is welcome! :)
+
+
+Basic installation
+~~~~~~~~~~~~~~~~~~
+
+To install, type::
+
+    # python setup.py install
+
+or use pip::
+
+    # pip install pidsim.models
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import pidsim_models
+
+setup(
+    name = 'pidsim.models',
+    version = pidsim_models.__version__,
+    license = pidsim_models.__license__,
+    description = pidsim_models.__description__,
+    long_description = open('README.rst').read(),
+    author = pidsim_models.__author__,
+    author_email = pidsim_models.__email__,
+    url = pidsim_models.__url__,
+    platforms = 'any',
+    packages = ['pidsim_models'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
